5fbf75ef4b2e33fa7310513b06ddf8304debd28b
bak0903 <bak0903@mail.ru>
2019-06-19 12:13:48 +0600

    Создал модели и провел миграции.

--- randomuser/webapp/models.py
+++ randomuser/webapp/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    gender = models.CharField(max_length=15)
+    name = models.ForeignKey('Name', on_delete=models.CASCADE)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    email = models.CharField(max_length=255)
+    dob = models.ForeignKey('Dob', on_delete=models.CASCADE)
+    registered = models.ForeignKey('Registered', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=255)
+    cell = models.CharField(max_length=255)
+    user_id = models.ForeignKey('Id', on_delete=models.CASCADE)
+    picture = models.ForeignKey('Picture', on_delete=models.CASCADE)
+    nat = models.CharField(max_length=3)
+
+
+class Name(models.Model):
+    title = models.CharField(max_length=255)
+    first = models.CharField(max_length=255)
+    last = models.CharField(max_length=255)
+
+
+class Location(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postcode = models.IntegerField()
+    coordinates = models.ForeignKey('Coordinates', on_delete=models.CASCADE)
+    timezone = models.ForeignKey('TimeZone', on_delete=models.CASCADE)
+
+
+class Coordinates(models.Model):
+    latitude = models.CharField(max_length=8)
+    longitude = models.CharField(max_length=8)
+
+
+class TimeZone(models.Model):
+    offset = models.CharField(max_length=5)
+    description = models.CharField(max_length=255)
+
+
+class Login(models.Model):
+    uuid = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    salt = models.CharField(max_length=255)
+    md5 = models.CharField(max_length=255)
+    sha1 = models.CharField(max_length=255)
+    sha256 = models.CharField(max_length=255)
+
+
+class Dob(models.Model):
+    date = models.CharField(max_length=20)
+    age = models.SmallIntegerField()
+
+
+class Registered(models.Model):
+    date = models.CharField(max_length=20)
+    age = models.SmallIntegerField()
+
+
+class Id(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Picture(models.Model):
+    large = models.CharField(max_length=255)
+    medium = models.CharField(max_length=255)
+    thumbnail = models.CharField(max_length=255)
